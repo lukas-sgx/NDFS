e498359fba03a41ec7d7562d89327da9678630b2
Lukas Soigneux <lukas.soigneux@gmail.com>
2025-08-25 20:52:05 +0200

    Notif manage

--- client/main.c
+++ client/main.c
@@ -51,8 +51,8 @@ int main(int argc, char const *argv[])
         bytes_read = recv(sock, buffer, BUFFER_SIZE - 1, 0);
         buffer[bytes_read] = '\0';
         if (strlen(buffer) > 0) {
-            notify_init("Notif Test");
-            NotifyNotification *notif = notify_notification_new("Hello", "Coucou depuis C", "dialog-information");
+            notify_init("ZOU!");
+            NotifyNotification *notif = notify_notification_new("Transport Région Sud", "LUKAS -> BUS n°836", "dialog-information");
 
             notify_notification_show(notif, NULL);
             g_object_unref(G_OBJECT(notif));
